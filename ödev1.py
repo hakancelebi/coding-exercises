5ab6e0606be87bd4160d4d1e37e383f66db1339e
hakancelebi <70070705+hakancelebi@users.noreply.github.com>
2020-08-23 04:29:24 +0300

    Add files via upload

---
+++ ödev1.py
@@ -0,0 +1,27 @@
+print("Sayı Çarpma")
+
+a = int(input("Birinci Sayı: "))
+b = int(input("İkinci Sayı: "))
+c = int(input("Üçüncü Sayı: "))
+
+print("Sayılar Çarpılıyor....")
+
+d = a * b * c
+
+print("{}*{}*{}={}".format(a,b,c,d))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
